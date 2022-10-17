b3e838f07259cbc31d7379595dfbef9d57c10797
Luke.Deng <yanyouchi@163.com>
2022-10-17 09:24:33 +0800

    add增加信息发送页面

--- src/main/java/com/example/wx/controller/UserController.java
+++ src/main/java/com/example/wx/controller/UserController.java
@@ -13,14 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class UserController {
 
-    @RequestMapping("/")
+    @RequestMapping("/")//主页 登录页和列表页
     public String index() {
         return "index";
     }
 
-    @RequestMapping("/login")
+    @RequestMapping("/login")//公众号绑定页面
     public String login(){
         return "add";
+    }
+
+    @RequestMapping("/send")// 编辑发送页面
+    public String send(){
+        return "send";
     }
 
 }

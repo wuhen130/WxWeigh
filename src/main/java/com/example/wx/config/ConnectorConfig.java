1086d806804d681c278d2453d40afb12ad003e0a
wuhen130 <1303514723@qq.com>
2022-10-22 16:58:46 +0800

    2.00版本封板

--- src/main/java/com/example/wx/config/ConnectorConfig.java
+++ src/main/java/com/example/wx/config/ConnectorConfig.java
@@ -1,41 +1,41 @@
-//package com.example.wx.config;
-//
-//import org.apache.catalina.Context;
-//import org.apache.catalina.connector.Connector;
-//import org.apache.coyote.http11.Http11NioProtocol;
-//import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-//import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-//import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class ConnectorConfig {
-//
-//    @Bean
-//    public ServletWebServerFactory servletWebServerFactory() {
-//        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint = new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection securityCollection = new SecurityCollection();
-//                securityCollection.addPattern("/*");
-//                securityConstraint.addCollection(securityCollection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//        factory.addAdditionalTomcatConnectors(redirectConnector());
-//        return factory;
-//    }
-//
-//    private Connector redirectConnector() {
-//        Connector connector = new Connector(Http11NioProtocol.class.getName());
-//        connector.setScheme("http");
-//        connector.setPort(8081);
-//        connector.setSecure(false);
-//        connector.setRedirectPort(443);
-//        return connector;
-//    }
-//}
+package com.example.wx.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConnectorConfig {
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(redirectConnector());
+        return factory;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(Http11NioProtocol.class.getName());
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+}
